0c4f41539dfcbdd3074fab4f9c4e6689303592f9
mxd2018 <1406988305@qq.com>
2018-01-05 21:06:50 +0800

    添加了一个tostring方法

--- src/main/java/com/qianfeng/maven/pojo/po/Student.java
+++ src/main/java/com/qianfeng/maven/pojo/po/Student.java
@@ -15,5 +15,10 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
 	
 }
