d423d5bb3d0f46703142dad24d8e0bdd3dee0f4f
mxd2018 <1406988305@qq.com>
2018-01-05 20:56:22 +0800

    初次提交

---
+++ src/test/java/com/qianfeng/maven/pojo/po/StudentTest.java
@@ -0,0 +1,14 @@
+package com.qianfeng.maven.pojo.po;
+
+import org.junit.Test;
+
+public class StudentTest {
+	
+	/*@Test
+	public void test1(){
+		Student student=new Student();
+		student.setId(1);
+		student.setName("ÕÅÈı");
+		System.out.println(student);
+	}*/
+}
